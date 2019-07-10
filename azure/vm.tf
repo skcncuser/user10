ed85180d1b3fc650d09db8f426b54dcf6c32e971
skcncuser <skcncuser@outlook.kr>
2019-07-10 07:29:51 +0000

    u.txt

--- azure/vm.tf
+++ azure/vm.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     os_profile_linux_config {
         disable_password_authentication = true
         ssh_keys {
-            path     = "/home/user10/.ssh/authorized_keys"
+            path     = "/home/azureuser/.ssh/authorized_keys"
             key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGPRwMAUFiADhVdLQoyjOyNWMxyAxjRRLgnm/zadJmAHm+Icj/7+1lzGgSWRTk9ia0gu5TbI/4Um3QkwBnhzlsEcsEBmoA7q1y9pBPI8KgmdfxIzNOLiZv/ehJQUERElvSeCnApOnTgguC7rc6VRBeWFPjlhRcUyNyQCpKUxE/HKKkZhV4vJBKf80TjOU6j0rXh3a7k0FKtqnyMQZgZqvmS/E0DMGkQXDXct06RhW8BDhImBLxEL/DmepSHDQ09lR9Za41BahY3BfS3Y0ezLmJjefdRjlc+EBb37X0BiNhwqfnPmUuaT+OmLrU48NiGdUKp0rp53xvIfn3UcHslIxl user10@cc-30cbce91-6467bcd76d-zjbhx"
         }
     }
