99fd1a13060e9f5ae98a36556ba3dab511a1428f
skcncuser <skcncuser@outlook.kr>
2019-07-10 06:44:38 +0000

    q!

---
+++ azure/main.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+    subscription_id = "e0fae348-f6c2-45f5-87b7-c41c22782d8f"
+    client_id       = "29c332a8-5536-45d8-a462-78cbb3d3cd03"
+    client_secret   = "KgX*[@R-Va7eDFWdiO9Ih0mloTEAnP4x"
+    tenant_id       = "96e3cac9-1ab3-436b-9f79-2a0a4b687f1b"
+}
---
+++ azure/resource.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "user10_final" {
+    name     = "user10_final"
+    location = "koreasouth"
+}
